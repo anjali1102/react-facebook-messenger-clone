67c933354061fdba1df3b1f7129971c9cd97cb13
Anjali Chauhan <ac.chauhan1102@gmail.com>
2020-11-19 22:16:25 +0530

    added files to master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Button, InputLabel, FormControl, Input } from "@material-ui/core";
+import "./App.css";
+import Message from "./Message";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([
+    { username: "anjali", text: "hey" },
+    { username: "ankush", text: "hello all" },
+  ]);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    setUsername(prompt("Please enter your username"));
+  }, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    setMessages([...messages, {username: username, text: input}]);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MESSENGER🚀 </h1>
+      <h2>Welcome {username}</h2>
+      <form>
+        <FormControl>
+          <InputLabel>Write something...</InputLabel>
+          <Input
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <Button
+            disabled={!input}
+            variant="contained"
+            color="secondary"
+            type="submit"
+            onClick={sendMessage}
+          >
+            Send
+          </Button>
+        </FormControl>
+      </form>
+
+      {messages.map((message) => (
+        <Message username={message.username} text={message.text} />
+      ))}
     </div>
   );
 }
